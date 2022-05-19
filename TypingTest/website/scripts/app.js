8a22416fe4d6882d720341fc565e347f07a13df5
nomae <noma3h@outlook.com>
2022-05-19 16:29:37 +0200

    making sentences from generated words

--- TypingTest/website/scripts/app.js
+++ TypingTest/website/scripts/app.js
@@ -15,7 +15,7 @@
 //     }
 // }
 
-const RANDOM_WORDS_API = "https://raw.githubusercontent.com/nmondon/mots-frequents/master/data/frequence.json"
+const RANDOM_WORDS_API = "../data/words.json"
 const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('inputQuote')
 const timerElement = document.getElementById('timer')
@@ -27,43 +27,65 @@ const modeToggle = {
     mode1:1,
     mode2:2
 }
+// SETTING WORDS COUNT FOR "WORDS" MODE
+const wordCount = {
+    words20:20,
+    words50:50,
+    words70:70
+}
 // SETTING TIME LIMIT OPTION WHEN IN TIMER MODE
 const timerLimit = {
     timer15:15,
     timer30:30,
     timer60:60
 }
+let wordsTable = []
+
 // INITIALIZING RESULT ATTRIBUTS
 let timer = 00
 let wpm = 00
 let acc = 00
 
+// DEFAULT VALUES
+let selectedWordCount = wordCount.words20
+let selectedTime = timerLimit.timer30
+
 
 // FETCHING DATA FROM JSON FILE USING FETCH.THEN PROMISE METHODE
 function getRandomWords() {
     let id = Math.floor(Math.random() * 1490)
-    fetch(RANDOM_WORDS_API)
-    .then((response) => {
-        return response.json()
-    })
+    return fetch(RANDOM_WORDS_API)
+    .then((response) => response.json())
     .then((data) => {
-        let word = data[id].label
+        data.forEach(line => {
+            quoteDisplayElement.innerText = line.line
+        });
     })
-    .catch(error => {
+    .catch(function(error) {
         // HANDLING ERRORS
+        console.log('Request error', error)
     })
 }
-// 
+async function genPhrase () {
+    // let words = await getRandomWords()
+    // words.forEach(element => {
+    //     Object.entries(element).forEach(([key, value]) => {
+    //         console.log(`${key} ${value}`)
+    //     })
+    //     console.log('-------------------')
+    // });
+}
 async function newQuote() {
     let word = await getRandomWords()
     quoteDisplayElement.innerHTML = ""
-
-    console.log(word)
     
-    for (character of word) {
-        const wordCharacter = document.createElement('span')
-        wordCharacter.className = "char"
-        wordCharacter.innerHTML = character
-        quoteDisplayElement.appendChild(wordCharacter)
-    }
+    console.log(word)
+
+    //  PUT EACH LETTER IN A SPAN ELEMENT SO ITS EASY TO COMPRARE THEM
+    // for (character of word) {
+    //     const wordCharacter = document.createElement('span')
+    //     wordCharacter.className = "char"
+    //     wordCharacter.innerHTML = character
+    //     quoteDisplayElement.appendChild(wordCharacter)
+    // }
 }
